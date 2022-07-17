29654679076c7de0e163970df638c2d5071042ab
Blue Jessen <canealisblue@gmail.com>
2022-07-17 15:23:43 -0700

    Add basic cypress tests

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,5 +1,21 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('Should welcome user with their name', () => {
+    cy.visit('localhost:3000')
+    cy.get('form').within(()=> {
+      cy.get('.name-input').type('Cece')
+    })
+    cy.get('header').contains('Cece')
+  })
+
+  it('Should should have a submit button', () => {
+    cy.get('button')
+  })
+
+  it('User must have both fields filled to submit form', () => {
+    
+  })
+
+  it('Should change views to users horoscope when they click submit with appropriate information', () => {
+
   })
 })
