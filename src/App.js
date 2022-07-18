b7777a97ea829ebf4042a7cce889d22ebd0ce519
Blue Jessen <canealisblue@gmail.com>
2022-07-17 18:08:50 -0700

    Add tests

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     this.state = {
       userName: '',
       userZodiac: '',
-      horoscope: null
+      horoscope: null,
+      error: ''
     }
   }
 
@@ -23,22 +24,37 @@ class App extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault()
+    if(!this.state.userZodiac || !this.state.userName) {
+      this.setState({
+        error: 'Please fill out all fields'
+      })
+      return
+    }
     const URL = `https://aztro.sameerkumar.website/?sign=${this.state.userZodiac}&day=today`;
     fetch(URL, {
       method: 'POST'
     }).then(response => response.json())
       .then(data => this.setState({
-      horoscope: data
+      horoscope: data,
+      error: ''
     }))
   }
 
+  resetHome = (event) => {
+    this.setState({
+      userName: '',
+      userZodiac: '',
+      horoscope: null
+    })
+  }
 
   render() {
   return (
     <>
       <header> Welcome {this.state.userName}!</header>
+      {this.state.error && <h1>{this.state.error}</h1>}
       {!this.state.horoscope ? <Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} userName={this.state.userName} userZodiac={this.state.userZodiac}/> :
-      <HoroscopeContainer horoscope={this.state.horoscope} />}
+      <HoroscopeContainer resetHome={this.resetHome} userName={this.state.userName} userZodiac= {this.state.userZodiac} horoscope={this.state.horoscope} />}
 
     </>
   )
