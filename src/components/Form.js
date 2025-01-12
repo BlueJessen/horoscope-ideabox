f35b34c95a52381a1118c16da14f954e8dea8e44
Blue Jessen <canealisblue@gmail.com>
2025-01-12 10:48:09 -0500

    Add name state

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 
 export const Form = () => {
+    const [name, setName] = useState('');
     const zodiacs = [
         'aquarius', 
         'aries', 
